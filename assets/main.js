f676919e6f12cc23304fae6b65782b6f1943b248
borozoro <borozoro@outlook.com>
2016-12-20 23:58:26 +0100

    Add AJAX-call

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
+	$.ajax({
+		url: 'https://www.codeschool.com/users/rijrij.json',
+		dataType: 'jsonp',
+		success: function (response) {
+			var courses = response.courses.completed;
+			renderBadges(courses);
+		}
+	});
 
-  // your code will go here
+	function renderBadges(courses) {
+		var $badges = $('#badges');
+		courses.forEach(function (course) {
 
+			var $div = $('<div></div>', {
+				'class': 'course'
+			}).appendTo($badges);
+
+			$('<h3></h3>', {
+				text: course.title
+			}).appendTo($div);
+
+			$('<img></img>', {
+				src: course.badge
+			}).appendTo($div);
+
+			$('<a></a>', {
+				'class': 'btn btn-primary',
+				href: course.url,
+				target: '_blank',
+				text: 'See Course'
+			}).appendTo($div);
+
+		});
+	}
 });
